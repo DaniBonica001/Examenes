accdd3e5b386ea46b20319f23382fe6487b72839
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2022-04-23 20:00:45 -0500

    handleChange and handleSubmit methods to submit the information to the api

--- pages/signUp.js
+++ pages/signUp.js
@@ -2,7 +2,49 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
+
 export default function Home() {
+
+    let data = {
+        name: '',
+        email: '',
+        password: '',
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        //console.log(data)
+        const response = await fetch('/api/accounts/verifyAccount', {
+            method: 'POST',
+            body: JSON.stringify({ data }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        const responseData = await response.json();
+    }
+
+
+    let handleChange = (event) => {
+         console.log(event.target.name);
+        switch (event.target.name) {
+            case 'name':
+                data.name = event.target.value;
+                break;
+            case 'email':
+                data.email = event.target.value;
+                break;
+            case 'password':
+                data.password = event.target.value;
+                break;
+            default:
+                break;
+        }
+    };
+
+
     return (
 
         <div className={styles.container}>
@@ -13,24 +55,25 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <h2 className={styles.title}>Sign Up</h2>
+                <form className='form' onSubmit={handleSubmit}>
+                    <h2 className={styles.title}>Sign Up</h2>
 
-                <div className={styles.grid}>
-                    <div className={styles.card}>
-                        <h2>Lets' create your account</h2>
+                    <div className={styles.grid}>
+                        <div className={styles.card}>
+                            <h2>Lets' create your account</h2>
 
-                        <input id="name" type="text" placeholder="Full Name" />
-                        <input id="username" type="text" placeholder="Username" />
-                        <input id="password" type="password" placeholder="Password" />
-                        <br />                    
-                        <button id="CreateAccountButton">Create</button>                                               
+                            <input id="name" type="text" name='name' placeholder="Full Name" onChange={handleChange} />
+                            <input id="email" type="email" name='email' placeholder="Email" onChange={handleChange} />
+                            <input id="password" type="password" name='password' placeholder="Password" onChange={handleChange}/>
+                            <input type="password" placeholder=" Confirm Password" />
+                            <br />
+                            <input type="submit" value="Create" />
+                        </div>
                     </div>
-
-                </div>
+                </form>
             </main>
-
-
         </div>
 
     )
 }
+
