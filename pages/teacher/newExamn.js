5e0dcf89c39db5e816777c84b5617fefc2dfd709
Santiago Arevalo Valencia <santiguitar2003@gmail.com>
2022-04-29 10:49:38 -0500

    QuestionBox component imported in newExamn class

--- pages/teacher/newExamn.js
+++ pages/teacher/newExamn.js
@@ -1,4 +1,6 @@
+import styles from '../../../styles/Home.module.css'
 import Head from 'next/head'
+import QuestionBox from '../../../components/QuestionsBox'
 
 export default function Home(data) {
     return (
